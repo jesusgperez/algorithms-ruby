c0e38e03f63d62749ed86e0078b2186a1ac97e17
Jesus Perez <jesus.perez@uplight.com>
2023-12-13 18:15:58 -0500

    Recursive search method for a linked list

--- data_structures/linked_list.rb
+++ data_structures/linked_list.rb
@@ -1,25 +1,41 @@
-require 'byebug'
 require_relative 'domain'
 
 
 class LinkedList
-    attr_accessor :head, :tail
+    attr_reader :head, :tail, :n
 
+    
     def initialize(head = nil, tail = nil)
         @head = head
         @tail = tail
+        @n = 0
     end
 
     def insert(data)
         if @head.nil?
             @head = LNode.new(data=data)
             @tail = @head
+            @n += 1
             return
         end
-        byebug
 
         @tail.next = LNode.new(data=data)
         @tail = @tail.next
+        @n += 1
         nil
     end
+
+    def search(data)
+        search_recursive(@head, data)
+    end
+
+    def search_recursive(node, data)
+        if node.nil? || node.data == data
+            return node
+        end
+
+        search_recursive(node.next, data)
+    end
+
+    private :search_recursive
 end
--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -12,10 +12,28 @@ RSpec.describe LinkedList do
             linked_list.insert(data=5)
             expect(linked_list.head.data).to eq(5)
             expect(linked_list.tail.data).to eq(5)
+            expect(linked_list.n).to eq(1)
             linked_list.insert(data=6)
-            byebug
             expect(linked_list.tail.data).to eq(6)
             expect(linked_list.head.next.data).to eq(6)
+            expect(linked_list.n).to eq(2)
+        end
+    end
+
+    describe '#linked_list-search' do
+        it 'Search an element in the linked list' do
+            linked_list = LinkedList.new
+            for i in 1..5 do
+                linked_list.insert(i)
+            end
+            expect(linked_list.head.data).to eq(1)
+            expect(linked_list.tail.data).to eq(5)
+
+            searched = linked_list.search(3)
+            expect(searched.data).to eq(3)
+            
+            searched = linked_list.search(6)
+            expect(searched).to eq(nil)
         end
     end
 end
